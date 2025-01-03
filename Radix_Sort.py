021b2cdf4cd5728cf54c5cf6b9f3c973acf664b0
Rajbir Ahmed <rajbirsakib81@gmail.com>
2025-01-03 22:51:09 +0600

    Sorting

---
+++ Radix_Sort.py
@@ -0,0 +1,41 @@
+def counting_sort(arr,exp):
+    n=len(arr)
+    output=[0]*n  
+    count =[0]*10 
+
+    # Count occurrences of digits
+    for i in range(n):
+        index=(arr[i] // exp)%10
+        count[index]+=1
+
+    # Update count[i] to store position of this digit in output array
+    for i in range(1,10):
+        count[i]+=count[i-1]
+
+    # Build the output array
+    for i in range(n-1,-1,-1):
+        index=(arr[i] // exp)%10
+        output[count[index]-1]=arr[i]
+        count[index]-=1
+
+    # Copy the sorted elements to the original array
+    for i in range(n):
+        arr[i]=output[i]
+
+
+def radix_sort(arr):
+    max_num=max(arr)
+    exp=1  # Exponent representing the current digit position
+
+    while max_num // exp >0:
+        counting_sort(arr,exp)
+        exp*=10
+
+try:
+    user_input=input("Enter the numbers: ")
+    arr=list(map(int,user_input.split()))
+    print("Original array: ",arr)
+    radix_sort(arr)
+    print("Sorted array: ",arr)
+except ValueError:
+    print("Invalid input!")
